ea7086b722ca6f270209dec70ca0f3c8aeafe27e
dzmitryvol4 <volchokdmitry@gmail.com>
2021-04-19 17:37:01 +0300

    delete some imports and comments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AutorizationComponent } from './auth/autorization/autorization.component';
 import { environment } from 'src/environments/environment';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
@@ -11,7 +9,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NewRegistrationUserComponent } from './new-registration-user/new-registration-user.component';
 import { AuthModule } from './auth/auth.module';
 import { PageModule } from './page/page.module';
-import { Checkinterseptor } from './check.interseptor';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthGuard } from './auth/auth.guard';
 import { AuthInterceptor } from './auth-interseptor';
@@ -31,7 +28,6 @@ import { AuthInterceptor } from './auth-interseptor';
   ],
   providers: [
     AuthGuard,
-    // { provide: HTTP_INTERCEPTORS, useClass: Checkinterseptor, multi: true },
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
   ],
   bootstrap: [AppComponent],
