aa32ca326b2bd81d60e8644e1bae1042a2202ff3
dzmitryvol4 <volchokdmitry@gmail.com>
2021-04-19 17:48:15 +0300

    delete import , console.log, local sorage value delete get empty path module comment value change(input)

--- src/app/auth/autorization/autorization.component.ts
+++ src/app/auth/autorization/autorization.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
@@ -19,26 +18,18 @@ export class AutorizationComponent implements OnInit {
   constructor(
     private afs: AngularFirestore,
     private router: Router,
-    private _http: HttpClient,
     private authGuardService: AuthGuardService
-  ) {
-    _http.get('/auth/auth.module').subscribe((result) => {
-      console.log(result);
-    });
-  }
+  ) {}
 
   getInfo(): void {
     let userDoc = this.afs.firestore.collection('users');
-    console.log(userDoc);
     userDoc.doc();
     this.name = this.fullNameControl.get('firstName').value;
     this.password = this.fullNameControl.get('passwordValue').value;
-    console.log(userDoc.doc(this.name).get());
     userDoc
       .doc(this.name)
       .get()
       .then((log) => {
-        console.log(log.data());
         if (log.exists && this.password === log.data().password) {
           if (this.name === this.password && this.name === 'admin') {
             this.routeAdmin();
@@ -50,23 +41,14 @@ export class AutorizationComponent implements OnInit {
   }
 
   routeOk(): void {
-    // this.router.navigateByUrl(`page/${this.role}`);
-    // localStorage.setItem('isAutintificated', 'true');
     this.authGuardService.setAuthSatatus = true;
     this.router.navigateByUrl(`page/content`);
-    console.log(this.role, 'aladldlldld');
   }
 
   routeAdmin(): void {
-    // localStorage.setItem('isAutintificated', 'true');
     this.authGuardService.setAuthSatatus = true;
     this.router.navigateByUrl(`page/admin`);
   }
-
-  // routeBack(): void {
-  //   this.router.navigate(['']);
-  // }
-
   nameControl: FormControl;
   loginControl: FormControl;
 
@@ -76,13 +58,11 @@ export class AutorizationComponent implements OnInit {
       passwordValue: new FormControl(),
     });
 
-    this.fullNameControl
-      .get('firstName')
-      .valueChanges.subscribe((value) => console.log(value));
+    this.fullNameControl.get('firstName');
+    //if you need to see value changes .valueChanges.subscribe((value) => console.log(value));
 
-    this.fullNameControl
-      .get('passwordValue')
-      .valueChanges.subscribe((value) => console.log(value));
+    this.fullNameControl.get('passwordValue');
+    //if you need to see value changes .valueChanges.subscribe((value) => console.log(value));
 
     this.nameControl = new FormControl('new', [
       Validators.required,
