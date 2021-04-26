94e1352a7e9d4fc9aa7dabb5c76cc00fb795477b
dzmitryvol4 <volchokdmitry@gmail.com>
2021-04-26 13:27:58 +0300

    new variable public name

--- src/app/testdev/testdev.component.ts
+++ src/app/testdev/testdev.component.ts
@@ -3,13 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-testdev',
   templateUrl: './testdev.component.html',
-  styleUrls: ['./testdev.component.css']
+  styleUrls: ['./testdev.component.css'],
 })
 export class TestdevComponent implements OnInit {
+  public name: string = 'hello world';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
